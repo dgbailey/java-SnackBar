a290a61e4de00ff2ddfb53bcdd6aadc93548a632
dgbailey <bailey.dust@gmail.com>
2019-04-22 13:04:17 -0700

    snack class added

---
+++ src/snackbar/Snack.java
@@ -0,0 +1,94 @@
+package snackbar;
+
+public class Snack
+{
+    private static int maxId = 0;
+    private int id;
+    private String name;
+    private int quantity;
+    double int cost;
+    private int vendingMachineId;
+
+
+    public Snack(int id, String name, int quantity,double cost,int vendingMachineId)
+    {
+        maxId++
+        this.id = maxId;
+
+        this.name = name;
+        this.quantity = quantity;
+        this.cost = cost;
+        this.vendingMachineId = vendingMachineId;
+
+    }
+
+    public getId(){
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public getCost()
+    {
+        return cost;
+    }
+
+    public void setCost(double cost)
+    {
+        this.cost = cost;
+    }
+    
+        
+
+    public getVendingMachineId()
+    {
+        return vendingMachineId;
+    }
+
+    public void setVendingMachineId(double cost)
+    {
+        this.vendingMachineId = vendingMachineId;
+    }
+
+    public getQuantity()
+    {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity)
+    {
+        this.quantity = quantity;
+    }
+
+    public void addQuantity(int amountAdded)
+    {
+        this.quantity = amountAdded + this.quantity;
+    }
+
+    public buySnack(int numberSnacks)
+    {
+        this.quantity = this.quantity - numberSnacks;
+    }
+
+    public totalCost(double quantSnacks)
+    {
+        return this.cost*quantSnacks;
+    }
+
+
+
+
+}
